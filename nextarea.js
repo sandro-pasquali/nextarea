fbaa8730ab5cd5e6cc253bfccb4f00d3d99f38fa
Sandro Pasquali <spasquali@gmail.com>
2012-06-18 19:20:34 -0400

    fixed up in order to accomodate multiple textareas.

--- nextarea.js
+++ nextarea.js
@@ -1,29 +1,30 @@
 (function($) {
 
  	$.fn.nextarea = function(options) {
+		this.each(function() {
+			var t = $(this);
+			t.css("overflow", "hidden");
 
- 		var t = this;
-		t.css("overflow", "hidden");
+			var min  	= t.height();
+			var dummy	= $('<div></div>').css({
+				position	: 'absolute',
+				top			: -5000,
+				left		: -5000,
+				width		: t.width(),
+				fontSize	: t.css('fontSize'),
+				fontFamily	: t.css('fontFamily'),
+				lineHeight	: t.css('lineHeight'),
+				resize		: 'none'
+			}).appendTo(document.body);
 
-		var min  	= t.height();
-		var dummy	= $('<div id="autoTextarea-' + t.attr("id") + '"></div>').css({
-			position	: 'absolute',
-			top			: -5000,
-			left		: -5000,
-			width		: t.width(),
-			fontSize	: t.css('fontSize'),
-			fontFamily	: t.css('fontFamily'),
-			lineHeight	: t.css('lineHeight'),
-			resize		: 'none'
-		}).appendTo(document.body);
+			var update = function() {
+				dummy.html(t.val().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;').replace(/\n/g, '<br/>'));
+				t.css('height', Math.max(dummy.height() + 30, min));
+			}
 
-		var update = function() {
-			dummy.html(t.val().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;').replace(/\n/g, '<br/>'));
-			t.css('height', Math.max(dummy.height() + 30, min));
-		}
-
-		t.change(update).keyup(update).keydown(update);
-		update();
+			t.change(update).keyup(update).keydown(update);
+			update();
+		});
  	}
 
 })(jQuery);
